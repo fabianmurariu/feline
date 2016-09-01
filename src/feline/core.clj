87d9f1caa811cf578459be6d0863bdd19f57b5e7
fabianmurariu <murariu.fabian@gmail.com>
2016-09-01 10:43:51 +0100

    added nested map an simplified fmap

--- src/feline/core.clj
+++ src/feline/core.clj
@@ -4,35 +4,55 @@
 ;; longer a seq, on maps the function is applied right handed aka on the value and it's eager
 
 (defmulti fmap
-  "Applies function f to each item in the data structure s and returns
-   a structure of the same kind."
+  "Applies function f to each item in the data structure s"
   {:arglists '([f s])}
-  (fn [f s] (type s)))
+  (fn [f s & n] (if (map? s) :map
+                    (if (coll? s) :coll))))
 
-(defmethod fmap clojure.lang.IPersistentVector
-  [f v]
-  (map (partial fmap f) v))
+(defmethod fmap :coll
+  ([f v]
+   (map (partial fmap f) v)))
 
-(defmethod fmap clojure.lang.IPersistentMap
-  [f m]
-  (into (empty m) (map (fn [[k v]] [k (fmap f v)]) m)))
+(defmethod fmap :map
+  ([f m]
+   (into (empty m) (map (fn [[k v]] [k (fmap f v)]) m))))
 
-(defmethod fmap clojure.lang.LazySeq
-  [f v]
-  (map (partial fmap f) v))
+(defmethod fmap :default
+  ([f v]
+   (f v)))
 
-(defmethod fmap clojure.lang.IPersistentList
-  [f v]
-  (map (partial fmap f) v))
+;; Nested map with count
 
-(defmethod fmap clojure.lang.IPersistentSet
-  [f s]
-  (map (partial fmap f) s))
+(defmulti nmap
+  "Applies function f to each item in the data structure s"
+  {:arglists '([f s n])}
+  (fn [f s ^java.lang.Long n]
+    (if (map? s)
+      [:map (= n 0)]
+      (if (coll? s)
+        [:coll (= n 0)]))))
 
-(defmethod fmap java.lang.Object
-  [f v]
+(defmethod nmap [:map false]
+  ([f v n]
+   (into (empty v) (map (fn [[k v1]] [k (nmap f v1 (- n 1))]) v))))
+
+(defmethod nmap [:coll false]
+  ([f c n]
+   (map #(nmap f % (- n 1)) c)))
+
+(defmethod nmap :default
+  [f v & n]
   (f v))
 
-(defmethod fmap nil
-  [_ _]
-  nil)
+
+(defn nmap1
+  "this applies f to the first level, it behaves similar to map (but it is eager on maps), it also applies to v on maps"
+  [f v] (nmap f v 1))
+
+(defn nmap2 [f v] (nmap f v 2))
+(defn nmap3 [f v] (nmap f v 3))
+(defn nmap4 [f v] (nmap f v 4))
+(defn nmap5 [f v] (nmap f v 5))
+(defn nmap6 [f v] (nmap f v 6))
+
+;; want more make your own
--- test/feline/core_test.clj
+++ test/feline/core_test.clj
@@ -4,7 +4,7 @@
 
 (deftest nested-fmap-test
   (testing "map over nil"
-    (is (= nil (fmap inc nil))))
+    (is (= true (fmap nil? nil))))
   (testing "map over any value"
     (is (= "BIGGER" (fmap clojure.string/upper-case "bigger")))
     (is (= 5 (fmap inc 4))))
@@ -19,4 +19,13 @@
   (testing "map over an array of maps"
     (is (= [{:a 2} {:b 3 :c 4}] (fmap inc [{:a 1} {:b 2 :c 3}]))))
   (testing "map over a map of maps"
-    (is (= {:a {:b 2} :b {:c 3}} (fmap inc {:a {:b 1} :b {:c 2}})))))
+    (is (= {:a {:b 2} :b {:c 3}} (fmap inc {:a {:b 1} :b {:c 2}}))))
+  (testing "inc all over no matter how deep"
+    (is (= [1 [[[2]]] [3]] (fmap inc [0 [[[1]]] [2]])))))
+
+(deftest nested-limited-fmap
+  (testing "map once over nested array"
+    (is (= [1 2 3] (nmap1 count [[1] [2 3] [4 5 6]]))))
+  (testing "map inside an array inside a nested map"
+    (is (= [{:a {:b 1 :c 2}} {:x {:b 2 :c 1} :y {}}]
+           (nmap3 count [{:a {:b [1] :c [1 2]}} {:x {:b [3 4] :c [5]} :y {}}])))))
